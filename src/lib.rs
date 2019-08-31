a0a6745d72f73f159590b9fbe97511161eb6f5b4
Xidorn Quan <me@upsuper.org>
2019-08-31 22:53:03 +1000

    Add more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub use default_boxed_derive::DefaultBoxed;
 
 /// Helper trait to create a boxed instance of the given type with a default value for each field.
 ///
-/// This trait can be derived.
+/// This trait can be derived for structs with named fields.
 ///
 /// To derive this trait, each field needs to also implement this trait, but all types which
 /// implements `Default` implements this trait via the blanket `impl` already.
@@ -10,6 +10,7 @@ pub use default_boxed_derive::DefaultBoxed;
 /// In addition, if a field is an array, only the item type needs to implement this trait, and each
 /// item would be initialized separately.
 pub trait DefaultBoxed: Sized {
+    /// Create a boxed instance with default value for each field.
     fn default_boxed() -> Box<Self> {
         let mut v = Vec::with_capacity(1);
         let raw = v.as_mut_ptr();
@@ -20,6 +21,14 @@ pub trait DefaultBoxed: Sized {
         }
     }
 
+    /// Fill the given memory location with default value.
+    ///
+    /// # Safety
+    ///
+    /// For callers, behavior is undefined if `ptr` is not valid for writes, or it is not properly
+    /// aligned.
+    ///
+    /// For impls, behavior is undefined if this method reads from `ptr`.
     unsafe fn default_in_place(ptr: *mut Self);
 }
 
