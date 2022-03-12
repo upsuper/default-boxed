06d3cb08e0f648eed151ac8881c16a224007ef8b
Xidorn Quan <me@upsuper.org>
2022-03-12 23:58:42 +1100

    Switch to use addr_of_mut macro

--- derive/lib.rs
+++ derive/lib.rs
@@ -1,11 +1,11 @@
 extern crate proc_macro;
 
-use proc_macro2::{Ident, Span, TokenStream};
+use proc_macro2::{Span, TokenStream};
 use quote::{quote, quote_spanned, ToTokens};
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
 use syn::token::Comma;
-use syn::{parse_quote, Data, DeriveInput, Fields, GenericParam, Index, Type};
+use syn::{parse_quote, Data, DeriveInput, Fields, Ident, Index, Type};
 
 #[proc_macro_derive(DefaultBoxed)]
 pub fn derive_default_boxed(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -14,7 +14,6 @@ pub fn derive_default_boxed(input: proc_macro::TokenStream) -> proc_macro::Token
 
 fn derive(mut input: DeriveInput) -> TokenStream {
     let name = &input.ident;
-    let generics = &input.generics;
     let data = match input.data {
         Data::Struct(data) => data,
         _ => {
@@ -24,44 +23,7 @@ fn derive(mut input: DeriveInput) -> TokenStream {
         }
     };
 
-    // Generate a struct with all fields wrapped with `MaybeUninit`.
-    let uninit_name = Ident::new(&format!("{}Uninit", name.to_string()), Span::call_site());
-    let uninit_def = quote! { struct #uninit_name #generics };
-    let uninit_struct = match &data.fields {
-        Fields::Named(fields) => {
-            let fields = fields.named.iter().map(|field| {
-                let name = field.ident.as_ref().unwrap();
-                let ty = wrap_maybe_uninit(&field.ty);
-                quote! { #name: #ty }
-            });
-            quote! { #uninit_def { #(#fields,)* } }
-        }
-        Fields::Unnamed(fields) => {
-            let fields = fields
-                .unnamed
-                .iter()
-                .map(|field| wrap_maybe_uninit(&field.ty));
-            quote! { #uninit_def (#(#fields),*); }
-        }
-        Fields::Unit => quote! { #uninit_def; },
-    };
-
-    // Generate the statement of a `MaybeUninit` struct of the given type.
-    let params = if generics.params.is_empty() {
-        quote! {}
-    } else {
-        let params = generics.params.iter().map(|param| match param {
-            GenericParam::Type(ty) => ty.ident.to_token_stream(),
-            GenericParam::Const(c) => c.ident.to_token_stream(),
-            GenericParam::Lifetime(l) => l.lifetime.to_token_stream(),
-        });
-        quote! { <#(#params),*> }
-    };
-    let transmute_uninit = quote! {
-        let uninit: &mut #uninit_name #params = ::core::mem::transmute(ptr);
-    };
-
-    // Generate code to write default value into the `MaybeUninit` reference.
+    // Generate code to write default value into the position pointed by the ptr.
     let fields = match data.fields {
         Fields::Named(fields) => fields.named.into_iter(),
         Fields::Unnamed(fields) => fields.unnamed.into_iter(),
@@ -74,7 +36,7 @@ fn derive(mut input: DeriveInput) -> TokenStream {
                 || Index::from(i).to_token_stream(),
                 |ident| ident.to_token_stream(),
             );
-            write_to_uninit(&quote!(uninit.#name), &field.ty)
+            write_to_uninit(&quote!((*ptr).#name), &field.ty, 0)
         })
         .collect();
 
@@ -94,37 +56,25 @@ fn derive(mut input: DeriveInput) -> TokenStream {
         #where_clause
         {
             unsafe fn default_in_place(ptr: *mut Self) {
-                #uninit_struct
-                #transmute_uninit
                 #write_default
             }
         }
     }
 }
 
-/// Wrap the given type with `MaybeUninit`.
-/// If the type is an array, wrap the inner type with `MaybeUninit`.
-fn wrap_maybe_uninit(ty: &Type) -> TokenStream {
+fn write_to_uninit(var: &TokenStream, ty: &Type, array_depth: usize) -> TokenStream {
     match ty {
         Type::Array(arr) => {
-            let elem = wrap_maybe_uninit(&arr.elem);
+            let array_depth = array_depth + 1;
+            let i = format!("i{array_depth}");
+            let i = Ident::new(&i, Span::call_site());
             let len = &arr.len;
-            quote! { [#elem; #len] }
-        }
-        Type::Tuple(tuple) => {
-            let elems = tuple.elems.iter().map(|elem| wrap_maybe_uninit(elem));
-            quote! { (#(#elems),*) }
-        }
-        _ => quote! { ::core::mem::MaybeUninit<#ty> },
-    }
-}
-
-fn write_to_uninit(var: &TokenStream, ty: &Type) -> TokenStream {
-    match ty {
-        Type::Array(arr) => {
-            let item = quote!(item);
-            let inner = write_to_uninit(&item, &arr.elem);
-            quote! { #var.iter_mut().for_each(|#item| { #inner }); }
+            let inner = write_to_uninit(&quote!(#var[#i]), &arr.elem, array_depth);
+            quote! {
+                for #i in 0..#len {
+                    #inner
+                }
+            }
         }
         Type::Tuple(tuple) => tuple
             .elems
@@ -132,14 +82,14 @@ fn write_to_uninit(var: &TokenStream, ty: &Type) -> TokenStream {
             .enumerate()
             .map(|(i, elem)| {
                 let idx = Index::from(i);
-                write_to_uninit(&quote!(#var.#idx), elem)
+                write_to_uninit(&quote!(#var.#idx), elem, array_depth)
             })
             .collect(),
         ty => {
             let call = quote_spanned! { ty.span() =>
                 <#ty as ::default_boxed::DefaultBoxed>::default_in_place
             };
-            quote! { #call(#var.as_mut_ptr()); }
+            quote! { #call(::core::ptr::addr_of_mut!(#var)); }
         }
     }
 }
